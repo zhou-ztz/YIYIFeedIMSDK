589dfc6b6162ec02c4cebafa9bc0ee61b9984c5f
tingzhizhou <tingzhi.zhou@yiartkeji.com>
2025-03-04 16:07:32 +0800

    2.4.8

---
+++ YIYIFeedIMSDK/TZImagePickerController/MyOCTest.h
@@ -0,0 +1,16 @@
+//
+//  MyOCTest.h
+//  YIYIFeedIMSDK
+//
+//  Created by yiyikeji_zhou on 2025/3/4.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyOCTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YIYIFeedIMSDK/TZImagePickerController/MyOCTest.m
@@ -0,0 +1,12 @@
+//
+//  MyOCTest.m
+//  YIYIFeedIMSDK
+//
+//  Created by yiyikeji_zhou on 2025/3/4.
+//
+
+#import "MyOCTest.h"
+
+@implementation MyOCTest
+
+@end
