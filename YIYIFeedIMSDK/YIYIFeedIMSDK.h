0693788536aa066cd1b91400c268546715f6ed22
tingzhizhou <tingzhi.zhou@yiartkeji.com>
2025-03-05 10:35:32 +0800

    2.5.3

--- YIYIFeedIMSDK/YIYIFeedIMSDK.h
+++ YIYIFeedIMSDK/YIYIFeedIMSDK.h
@@ -15,8 +15,6 @@ FOUNDATION_EXPORT const unsigned char YIYIFeedIMSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <YIYIFeedIMSDK/PublicHeader.h>
 
-#import <YIYIFeedIMSDK/MyOCTest.h>
-
 
 
 
